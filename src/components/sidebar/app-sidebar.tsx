db496050e09f9e6ef54528bcc0519a795e12e0c8
cardosofiles <cardosofiles@outlook.com>
2025-04-04 11:55:35 -0300

    feat: configuring shadcn ui with dark and light themes

---
+++ src/components/sidebar/app-sidebar.tsx
@@ -0,0 +1,178 @@
+"use client";
+
+import {
+  IconCamera,
+  IconChartBar,
+  IconDashboard,
+  IconDatabase,
+  IconFileAi,
+  IconFileDescription,
+  IconFileWord,
+  IconFolder,
+  IconHelp,
+  IconLayoutDashboard,
+  IconListDetails,
+  IconReport,
+  IconSearch,
+  IconSettings,
+} from "@tabler/icons-react";
+import Link from "next/link"; // Importação do Link do Next.js
+import type * as React from "react";
+
+import { NavDocuments } from "@/components/sidebar/nav-documents";
+import { NavMain } from "@/components/sidebar/nav-main";
+import { NavSecondary } from "@/components/sidebar/nav-secondary";
+import { NavUser } from "@/components/sidebar/nav-user";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+const avatarUrl = "https://github.com/Cardosofiles.png";
+
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: avatarUrl,
+  },
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "/dashboard", // Rota real do App Router
+      icon: IconDashboard,
+    },
+    {
+      title: "Lifecycle",
+      url: "/lifecycle",
+      icon: IconListDetails,
+    },
+    {
+      title: "Analytics",
+      url: "/analytics",
+      icon: IconChartBar,
+    },
+    {
+      title: "Projects",
+      url: "/projects",
+      icon: IconFolder,
+    },
+  ],
+  navClouds: [
+    {
+      title: "Capture",
+      icon: IconCamera,
+      isActive: true,
+      url: "/capture",
+      items: [
+        {
+          title: "Active Proposals",
+          url: "/capture/active",
+        },
+        {
+          title: "Archived",
+          url: "/capture/archived",
+        },
+      ],
+    },
+    {
+      title: "Proposal",
+      icon: IconFileDescription,
+      url: "/proposal",
+      items: [
+        {
+          title: "Active Proposals",
+          url: "/proposal/active",
+        },
+        {
+          title: "Archived",
+          url: "/proposal/archived",
+        },
+      ],
+    },
+    {
+      title: "Prompts",
+      icon: IconFileAi,
+      url: "/prompts",
+      items: [
+        {
+          title: "Active Proposals",
+          url: "/prompts/active",
+        },
+        {
+          title: "Archived",
+          url: "/prompts/archived",
+        },
+      ],
+    },
+  ],
+  navSecondary: [
+    {
+      title: "Settings",
+      url: "/settings",
+      icon: IconSettings,
+    },
+    {
+      title: "Get Help",
+      url: "/help",
+      icon: IconHelp,
+    },
+    {
+      title: "Search",
+      url: "/search",
+      icon: IconSearch,
+    },
+  ],
+  documents: [
+    {
+      name: "Data Library",
+      url: "/library",
+      icon: IconDatabase,
+    },
+    {
+      name: "Reports",
+      url: "/reports",
+      icon: IconReport,
+    },
+    {
+      name: "Word Assistant",
+      url: "/word-assistant",
+      icon: IconFileWord,
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:!p-1.5"
+            >
+              <Link href="/">
+                <IconLayoutDashboard className="!size-5" />
+                <span className="text-base font-semibold">Página Inicial</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        <NavDocuments items={data.documents} />
+        <NavSecondary items={data.navSecondary} className="mt-auto" />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
---
+++ src/components/sidebar/site-header.tsx
@@ -0,0 +1,25 @@
+import { ThemeSelector } from "@/components/theme/theme-selector";
+import { Separator } from "@/components/ui/separator";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { ModeToggle } from "../theme/theme-toggle";
+
+export function SiteHeader() {
+  return (
+    <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+      <div className="flex w-full items-center text-center gap-1 px-4 lg:gap-2 lg:px-6">
+        <SidebarTrigger className="-ml-1" />
+        <Separator
+          orientation="vertical"
+          className="mx-2 data-[orientation=vertical]:h-4"
+        />
+        <h1 className="text-base flex font-medium text-center">
+          Registration Event
+        </h1>
+        <div className="ml-auto flex items-center gap-2">
+          <ThemeSelector />
+          <ModeToggle />
+        </div>
+      </div>
+    </header>
+  );
+}
